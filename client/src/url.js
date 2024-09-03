d7fae5e1e4a0b86c8bd5c8311e8fca9ec58280b9
AnupShrestha28 <anupstha1068@gmail.com>
2024-09-03 18:11:44 +0545

    code refactored

--- client/src/url.js
+++ client/src/url.js
@@ -1,6 +1,6 @@
-// export const URL=import.meta.env.VITE_URL
-// export const IF=import.meta.env.VITE_IF
+export const URL=import.meta.env.VITE_URL
+export const IF=import.meta.env.VITE_IF
 
 
-export const URL="https://blog-app-tzru.onrender.com/"
-export const IF="https://blog-app-tzru.onrender.com/images/"
+// export const URL="https://blog-app-tzru.onrender.com/"
+// export const IF="https://blog-app-tzru.onrender.com/images/"
